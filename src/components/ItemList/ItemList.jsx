8bb8315b1d1698da0416e48e2f5d645d249a32de
FranciscoZaragoza10 <93441330+FranciscoZaragoza10@users.noreply.github.com>
2022-03-19 07:47:53 -0600

    fix containers

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,3 +1,14 @@
-function ItemList({ items }) {
+import Item from "../Item/Item";
+
+function ItemList({ prods }) {
+  return (
+    <>
+      {prods.map((prod) => (
+        <Item key={prod.id} prod={prod} />
+      ))}
+    </>
+  );
+
   // Desarrolla la vista utilizando un array de items y un map
 }
+export default ItemList;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { gFetch } from "../../helpers/gFetch";
-import ItemCount from "../ItemCount/ItemCount";
 
-const ItemListContainer = ({ saludo }) => {
+import ItemCount from "../ItemCount/ItemCount";
+import ItemList from "../ItemList/ItemList";
+
+export const ItemListContainer = ({ saludo }) => {
   const [bool, setbool] = useState(true);
   const [loading, setloading] = useState(true);
   const [prods, setprods] = useState([]);
@@ -17,31 +19,11 @@ const ItemListContainer = ({ saludo }) => {
     <>
       <div className="px-3">
         <h1>{saludo}</h1>
-        {loading ? (
-          <h2>Cargando...</h2>
-        ) : (
-          prods.map((prod) => (
-            <div key={prod.id}>
-              <div className="d-flex flex-wrap-column col-lg-6 col-md-7 col-7 shadow-lg align-items-center my-2 mx-5 carrito_prod">
-                <img
-                  className="rounded img__carrito"
-                  src={prod.src}
-                  alt="bolsa"
-                />
-                <h3 className="text-primary fs-5 mx-2 my-0 text-center w-25">
-                  {prod.nombre}
-                </h3>
-                <h4 className="fw-bold shadow-sm bg-white rounded mx-5">
-                  <i className="fas fa-shopping-bag text-primary"></i> $
-                  {prod.precio}
-                </h4>
-              </div>
-            </div>
-          ))
-        )}
+        {loading ? <h2>Cargando...</h2> : <ItemList prods={prods} />}
       </div>
       <ItemCount stock={5} initial={1} />
     </>
   );
 };
+
 export default ItemListContainer;
